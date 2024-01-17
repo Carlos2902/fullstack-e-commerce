4a960a7d7dc9c65f15bbd916aec6fda760728597
Carlos Lopez <carloslopezr29@gmail.com>
2024-01-17 14:28:45 -0500

    Product images added

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -14,8 +14,16 @@ const Filters = ({ categories }: {categories: Category[]}) => {
   const { categoryFilters, sort, setCategoryFilters, setSort} =
   useFilter()
 
-  const handleCategories = (categoryId: string) => {}
-  const handleSort = () => {}
+  const handleCategories = (categoryId: string) => {
+    if(categoryFilters.includes(categoryId)){
+      const updatedCategories = categoryFilters.filter((id)=>id!==categoryId)
+
+      setCategoryFilters (updatedCategories)
+    } else{
+      setCategoryFilters([...categoryFilters, categoryId])
+    }
+  }
+  const handleSort = (value: string) => setSort(value)
 
   return (
     <div className={classes.filters}>
