90aa22791a7b4a1241a1abb824de015ffc252e8a
Carlos Lopez <carloslopezr29@gmail.com>
2024-01-22 22:14:56 -0500

    CartItem renderized on CartPage

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import classes from './index.module.scss'
+
 function CartItem() {
-  return (
-    <div>CartItem</div>
-  )
+  return  <li className={classes.item}>
+        <h6>TITLE</h6>
+    </li>
+  
 }
 
 export default CartItem
