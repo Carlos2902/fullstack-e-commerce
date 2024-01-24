652328e5158295778bb5945a15a004cb6977611f
Carlos Lopez <carloslopezr29@gmail.com>
2024-01-24 14:06:41 -0500

    Implemented wonderful inline minimalistic checkout

---
+++ src/app/(pages)/checkout/CheckoutItem/index.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+
+import { Media } from '../../../_components/Media'
+import { Price } from '../../../_components/Price'
+
+import classes from './index.module.scss'
+
+export const CheckoutItem = ({ product, title, metaImage, quantity, index }) => {
+  return (
+    // Conditional: Product Image or Place Holder 'No image'
+    <li className={classes.item} key={index}>
+      <Link href={`/products/${product.slug}`} className={classes.mediaWrapper}>
+        {!metaImage && <span>No image</span>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media className={classes.media} imgClassName={classes.image} resource={metaImage} fill />
+        )}
+      </Link>
+        {/* item Details */}
+      <div className={classes.itemDetails}>
+        <div className={classes.titleWrapper}>
+          <h6>{title}</h6>
+          <Price product={product} button={false} />
+        </div>
+        <p className={classes.quantity}>x{quantity}</p>
+      </div>
+
+    {/* subTotal */}
+      <div className={classes.subtotal}>
+        <Price product={product} button={false} quantity={quantity} />
+      </div>
+    </li>
+  )
+}
